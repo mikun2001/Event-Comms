c318f20ce06b9b3e5316a5651839eaad2106218e
Abhilash Dash <abhilashdash2023@gmail.com>
2024-06-27 16:13:00 +0530

    renaming

--- force-app/main/default/lwc/numerator/numerator.js
+++ force-app/main/default/lwc/numerator/numerator.js
@@ -1,8 +1,19 @@
 import { LightningElement, api } from 'lwc';
 
 export default class Numerator extends LightningElement {
-    @api counter = 0;
-    handleIncrement() {
+    // @api counter = 0;
+
+    _currentCount = 0;
+    priorCount = 0;
+    @api
+    get counter() {
+      return this._currentCount;
+    }
+    set counter(value) {
+      this.priorCount = this._currentCount;
+      this._currentCount = value;
+    }
+        handleIncrement() {
       this.counter++;
     }
     handleDecrement() {
